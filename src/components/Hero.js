b7134e7b189fc4ac15df347c5007ad194d303311
imbrikis <imbrikis@yahoo.com>
2021-03-12 11:51:34 -0700

    styling: Hero comp

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import Image from 'gatsby-image'
-// ...GatsbyImageSharpFluid
 
-const Hero = () => {
-  return <h2>hero component</h2>
+const query = graphql`
+  {
+    person: file(relativePath: { eq: "person.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+const Hero = ({ showPerson }) => {
+  const data = useStaticQuery(query)
+  const imageData = data.person.childImageSharp.fluid
+
+  return (
+    <header className="hero">
+      {showPerson && <Image fluid={imageData} className="hero-person" />}
+    </header>
+  )
 }
 
 export default Hero
